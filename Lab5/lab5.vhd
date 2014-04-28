2e57d77434269f628f75115c3585e76d2921d52e
eXamadeus <exmachina445@gmail.com>
2014-04-28 09:04:22 -0400

    IM STUPID

--- Lab5/lab5.vhd
+++ Lab5/lab5.vhd
@@ -149,7 +149,7 @@ begin
 				case ir_out(8 downto 7) is
 					-- mv or mvi
 					when "00" => -- store bus into register XXX and finish
-						loadreg(to_integer(unsigned(ir_out(5 downto 3)))) <= 1; -- please work
+						loadreg(to_integer(unsigned(ir_out(5 downto 3)))) <= '1'; -- please work
 						-- case ir_out(5 downto 3) is
 						-- 	when "000" =>
 						-- 		loadreg0 <= 1;
