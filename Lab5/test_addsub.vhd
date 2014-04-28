47608b756766c0a164e4571ef0dcee00daa759ba
eXamadeus <exmachina445@gmail.com>
2014-04-28 11:46:27 -0400

    more ulogic lol

--- Lab5/test_addsub.vhd
+++ Lab5/test_addsub.vhd
@@ -9,22 +9,22 @@ end entity test_addsub_16;
 architecture b_test_addsub_16 of test_addsub_16 is
 	component addsub_16 is
 		port (
-			subcont	: in	std_ulogic;
-			carryo	: out	std_ulogic;
-			inputa	: in	std_ulogic_vector(15 downto 0);
-			inputb	: in	std_ulogic_vector(15 downto 0);
-			output	: out	std_ulogic_vector(15 downto 0)
+			subcont	: in	std_logic;
+			carryo	: out	std_logic;
+			inputa	: in	std_logic_vector(15 downto 0);
+			inputb	: in	std_logic_vector(15 downto 0);
+			output	: out	std_logic_vector(15 downto 0)
 		);
 	end component;
 
 	signal clockt	: std_logic;
 	signal clk_r	: std_logic := '1';
 
-	signal addsub	: std_ulogic;
-	signal carry	: std_ulogic;
-	signal busa_16	: std_ulogic_vector(15 downto 0) := (others => '0');
-	signal busb_16	: std_ulogic_vector(15 downto 0) := (others => '0');
-	signal out_test	: std_ulogic_vector(15 downto 0);
+	signal addsub	: std_logic;
+	signal carry	: std_logic;
+	signal busa_16	: std_logic_vector(15 downto 0) := (others => '0');
+	signal busb_16	: std_logic_vector(15 downto 0) := (others => '0');
+	signal out_test	: std_logic_vector(15 downto 0);
 
 begin
 
