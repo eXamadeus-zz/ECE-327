0e5e308c33f34cad4e022ed6ea8294ca4b0e2662
eXamadeus <exmachina445@gmail.com>
2014-04-28 09:40:53 -0400

    maybe?

--- Lab5/tb_lab5.vhd
+++ Lab5/tb_lab5.vhd
@@ -37,13 +37,13 @@ begin
 		resett	<= '1';
 		runt	<= '0';
 		dint	<= "0000000000000000";
-		wait until rising_edge(clockt);
+		wait until rising_edge(clockt); wait for 1 ns;
 		wait until rising_edge(clockt);
 		resett	<= '0';
 		wait until rising_edge(clockt);
 		resett	<= '1';
 
-		wait until rising_edge(clockt);
+		wait until rising_edge(clockt); wait for 1 ns;
 		wait until rising_edge(clockt);
 
 		--move 0x0001 into register 0
